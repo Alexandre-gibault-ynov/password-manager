3cbdf69c3782f9cc67b23024a0e547eb6d107c8b
Alexandre Gibault <alexandre.gibault@ynov.com>
2024-07-08 16:14:58 +0200

    [FEAT] MainFrame class implementation

--- src/main/java/org/example/MainFrame.java
+++ src/main/java/org/example/MainFrame.java
@@ -1,4 +1,12 @@
 package org.example;
 
-public class MainFrame {
+import javax.swing.*;
+
+public class MainFrame extends JFrame {
+    public MainFrame() {
+        setTitle("Password Manager");
+        setSize(600, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }
 }
