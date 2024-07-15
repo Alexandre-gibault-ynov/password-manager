56cbb1b41976dc1e31cf82660f8f8cd5b85e4f1f
Alexandre Gibault <alexandre.gibault@ynov.com>
2024-07-15 14:05:22 +0200

    [ADD] CredentialControllerTest class

---
+++ src/test/java/org/example/controller/CredentialControllerTest.java
@@ -0,0 +1,16 @@
+package org.example.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CredentialControllerTest {
+
+    @Test
+    void saveCredential() {
+    }
+
+    @Test
+    void generatePassword() {
+    }
+}
