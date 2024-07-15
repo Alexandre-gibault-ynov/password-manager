214e4448ae885d0a41d4b2b8f70f76b626451d05
Alexandre Gibault <alexandre.gibault@ynov.com>
2024-07-15 11:44:09 +0200

    [ADD] CredentialTest class

---
+++ src/test/java/org/example/model/CredentialTest.java
@@ -0,0 +1,40 @@
+package org.example.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CredentialTest {
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void setName() {
+    }
+
+    @Test
+    void getIdentifier() {
+    }
+
+    @Test
+    void setIdentifier() {
+    }
+
+    @Test
+    void getPassword() {
+    }
+
+    @Test
+    void setPassword() {
+    }
+
+    @Test
+    void getAuthService() {
+    }
+
+    @Test
+    void setAuthService() {
+    }
+}
