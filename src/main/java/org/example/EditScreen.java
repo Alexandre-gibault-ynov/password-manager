2d7d08ad15b5bcac227919da1f3b78d5e01f6925
Alexandre Gibault <alexandre.gibault@ynov.com>
2024-07-15 09:59:05 +0200

    [FEAT] Using PasswordGeneratorPanel class in edit screen

--- src/main/java/org/example/EditScreen.java
+++ src/main/java/org/example/EditScreen.java
@@ -1,12 +1,7 @@
 package org.example;
 
-import org.passay.CharacterRule;
-import org.passay.EnglishCharacterData;
-import org.passay.PasswordGenerator;
-
 import javax.swing.*;
 import java.awt.*;
-import java.util.Arrays;
 
 public class EditScreen extends JPanel {
     private LabeledTextField credentialName;
@@ -34,52 +29,6 @@ public class EditScreen extends JPanel {
         identifierField = new LabeledTextField("Identifier",20);
         passwordField = new LabeledTextField("Password",20);
         authServiceField = new LabeledTextField("Auth service",20);
-        passwordGeneratorOutputField = new JTextField(100);
-        passwordGeneratorOutputField.setEditable(false);
-
-
-        JPanel passwordGeneratorPanel = new JPanel(new GridBagLayout());
-
-        JLabel passwordGeneratorLabel = new JLabel("Password Generator");
-
-        JSlider passwordGeneratorSlider = new JSlider(JSlider.HORIZONTAL, 1, 100, 14);
-        passwordGeneratorSlider.setMajorTickSpacing(10);
-        passwordGeneratorSlider.setMinorTickSpacing(1);
-        JLabel charactersCountLabel = new JLabel(passwordGeneratorSlider.getValue() + " Characters");
-
-        passwordGeneratorSlider.addChangeListener(e -> {
-            int length = passwordGeneratorSlider.getValue();
-            charactersCountLabel.setText(passwordGeneratorSlider.getValue() + " Characters");
-            passwordGeneratorOutputField.setText(generatePassword(length));
-        });
-
-
-        JButton generatePasswordButton = new JButton("Generate");
-        generatePasswordButton.addActionListener(e -> {
-            String generatedPassword = generatePassword(passwordGeneratorSlider.getValue());
-            passwordGeneratorOutputField.setText(generatedPassword);
-        });
-
-        GridBagConstraints passwordGeneratorPanelConstraints = new GridBagConstraints();
-        passwordGeneratorPanelConstraints.insets = new Insets(5, 5, 5, 5);
-        passwordGeneratorPanelConstraints.fill = GridBagConstraints.HORIZONTAL;
-        passwordGeneratorPanelConstraints.gridx = 0;
-        passwordGeneratorPanelConstraints.gridy = 0;
-        passwordGeneratorPanel.add(passwordGeneratorLabel, passwordGeneratorPanelConstraints);
-
-        passwordGeneratorPanelConstraints.gridy++;
-        passwordGeneratorPanel.add(passwordGeneratorSlider, passwordGeneratorPanelConstraints);
-
-        passwordGeneratorPanelConstraints.gridx++;
-        passwordGeneratorPanel.add(charactersCountLabel, passwordGeneratorPanelConstraints);
-
-        passwordGeneratorPanelConstraints.gridx++;
-        passwordGeneratorPanel.add(generatePasswordButton, passwordGeneratorPanelConstraints);
-
-        passwordGeneratorPanelConstraints.gridy++;
-        passwordGeneratorPanelConstraints.gridx = 0;
-        passwordGeneratorPanelConstraints.gridwidth = 4;
-        passwordGeneratorPanel.add(passwordGeneratorOutputField, passwordGeneratorPanelConstraints);
 
         GridBagConstraints mainPanelConstraints = new GridBagConstraints();
         mainPanelConstraints.fill = GridBagConstraints.HORIZONTAL;
@@ -99,7 +48,7 @@ public class EditScreen extends JPanel {
         mainPanelConstraints.gridy++;
         mainPanelConstraints.gridx = 0;
         mainPanelConstraints.gridwidth = 2;
-        mainPanel.add(passwordGeneratorPanel, mainPanelConstraints);
+        mainPanel.add(new PasswordGeneratorPanel(), mainPanelConstraints);
 
         JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
         JButton backButton = new JButton("Back");
@@ -126,15 +75,5 @@ public class EditScreen extends JPanel {
         mainPanel.add(buttonPanel, mainPanelConstraints);
 
         add(mainPanel, BorderLayout.CENTER);
-    }
-
-    private String generatePassword(int length) {
-        PasswordGenerator generator = new PasswordGenerator();
-        CharacterRule lowerCaseRule = new CharacterRule(EnglishCharacterData.LowerCase, 1);
-        CharacterRule upperCaseRule = new CharacterRule(EnglishCharacterData.UpperCase, 1);
-        CharacterRule digitRule = new CharacterRule(EnglishCharacterData.Digit, 1);
-        CharacterRule specialCharRule = new CharacterRule(EnglishCharacterData.Special, 1);
-
-        return length >= 4 ? generator.generatePassword(length, Arrays.asList(lowerCaseRule, upperCaseRule, digitRule, specialCharRule)) : generator.generatePassword(length);
     }
 }
